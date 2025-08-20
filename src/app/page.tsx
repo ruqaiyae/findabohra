a67cc0da0ae76206a797eb5c27d56869c1d46983
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-08-19 19:36:23 -0700

    feat/hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <div className="min-h-screen bg-white">
       <Header />
-    </main>
+      <Hero />
+    </div>
   );
 }
